889a19dcd7a89e68dacee587288ba3ed25531d04
EricoDeMecha <techcider4134@gmail.com>
2020-02-01 22:04:58 +0300

    Error of size of the array due to memory management

--- fFile.m
+++ fFile.m
@@ -44,32 +44,54 @@ if sl <= pq
            L3  =coupler; % coupler
            L4 = follower; % Follower
            L1 = fixed_link; % Fixed_link
-          
-       for O2 = 0: ((1/36)*pi) : (2*pi)
+          % counters
+           i = 1; j  = 1; k = 0;
            % initial values of O3 and O4
-           O3 = (1/90)*pi; O4 = (1/60)*pi;
-           while(1)
+           nr = (1/3)*pi; nf = (7/18)*pi;
+           % arrays for storing the values
+           Datanr(i) = rad2deg(nr); Datanf(j) = rad2deg(nf);
+       for nw = 0: ((1/36)*pi): (2*pi)
                 % iterative functions
-                f1= L2*cos(O2) + L3 * cos(O3) - L4*cos(O4) - L1;
-                f2= L2*sin(O2) + L3 * sin(O3) - L4*sin(O4);
-                f1f2 = [f1;f2]; % functions matrix
-                disp(["O3:",O3," O4:",O4,"f1:",f1," f2:",f2]);
+                f_one= L2*cos(nw) + L3 * cos(nr) - L4*cos(nf) - L1;
+                f_two= L2*sin(nw) + L3 * sin(nr) - L4*sin(nf);
                 % check if those functions values are close enough to zero
-                temp_f1 = round(f1,5);temp_f2 = round(f2,5);
-                if(temp_f1 == 0) || (temp_f2 == 0)
-                    disp("----------------------------------");
-                    break
-                end
+                temp_fone = round(f_one,10);temp_ftwo = round(f_two,10);
+               
+           while((temp_fone ~= 0) || (temp_ftwo ~= 0))
+                % Position Analysis
+                % iterative functions
+                f_one= L2*cos(nw) + L3 * cos(nr) - L4*cos(nf) - L1;
+                f_two= L2*sin(nw) + L3 * sin(nr) - L4*sin(nf);
+                
+                f1f2 = [f_one;f_two]; % functions matrix
+                disp(["O3:",nr," O4:",nf,"f1:",f_one," f2:",f_two]);
+                
+                temp_fone = round(f_one,10);temp_ftwo = round(f_two,10);
+                
                 % Jacobian Matrix
-                a = -1 * L3 * sin(O3); b = L4 * sin(O4); c = L3*cos(O3);
-                d = -1 * L4 * cos(O4);
+                a = -1 * L3 * sin(nr); b = L4 * sin(nf); c = L3*cos(nr);
+                d = -1 * L4 * cos(nf);
                 Jacobi = [a b;c d];
                 CO3O4 = -1*(Jacobi\f1f2);
-                % new values of O3 and O4
-                O3 =O3 + CO3O4(1);
-                O4 =O4 + CO3O4(2);
-            end
+                % new values of O3= nr and O4 = nf
+     
+                nr =nr + CO3O4(1);
+                nf =nf + CO3O4(2);
+           end
+            disp("----------------------------------------------");
+            k = k + 1;
+            i = i + 1; j = j + 1;
+            Datanr(i) = rad2deg(nr);
+            Datanf(j) = rad2deg(nf);
        end
+       % clean up duplicates and print out the result
+       % Duplicates occur due to memory management of MATLAB
+       fnr = unique(Datanr, 'stable');
+       disp(fnr);
+       disp("---------");
+       fnf = unique(Datanf,'stable');
+       disp(fnf);
+       disp(k);
    end
 else
    %  double rocker mechanism
